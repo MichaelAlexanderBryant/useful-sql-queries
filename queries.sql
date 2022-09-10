28e939776459cc49d44192a8b33123a0500f90c4
Michael Bryant <58651380+MichaelABryant@users.noreply.github.com>
2022-09-10 10:43:31 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
-SELECT MAX(CASE WHEN rownum <= cnt*.25 THEN total_cases END) AS first_quantile,
-		MAX(CASE WHEN rownum <= cnt*.5 THEN total_cases END) AS second_quantile,
-		MAX(CASE WHEN rownum <= cnt*.75 THEN total_cases END) AS third_quantile
+-- Calculate quantiles.
+SELECT MAX(CASE WHEN rownum <= cnt*.25 THEN <col> END) AS first_quantile,
+		MAX(CASE WHEN rownum <= cnt*.5 THEN <col> END) AS second_quantile,
+		MAX(CASE WHEN rownum <= cnt*.75 THEN <col> END) AS third_quantile
 FROM
-(SELECT ROW_NUMBER() OVER (ORDER BY ISNULL(total_cases,0)) AS rownum, COUNT(*) OVER() AS cnt, ISNULL(total_cases,0) AS total_cases
-FROM CovidCases) sq;
+(SELECT ROW_NUMBER() OVER (ORDER BY <col>) AS rownum, COUNT(*) OVER() AS cnt, <col>
+FROM <tbl>) sq;
